af44d24de40d5ee1728eeb12ccf5ea44627cb072
Francisco Cerezo <fcb0007@alu.medac.es>
2025-05-09 17:23:58 +0200

    Actualizo el main

--- Main.py
+++ Main.py
@@ -2,49 +2,67 @@ from Digipymon import Digipymon
 from Enemigo import Enemigo
 from Inventario import Inventario
 from ListaNombres import ListaNombres
+from Jugador import Jugador
 import random
-def main ():
+
+
+def generar_digipymon_aleatorio():
     lista_nombres1 = ListaNombres()
-
-    def generar_digipymon_aleatorio():
-        tipos = ["fuego", "agua", "planta"]
-        nombre = lista_nombres1.obtener_nombre_digipymon()
-        vida = random.randint(10, 20)
-        ataque = random.randint(1, 10)
-        tipo = random.choice(tipos)
-        nivel = random.randint(1, 3)
-        digipymon1 = Digipymon(nombre, vida, ataque, tipo, nivel)
-        return digipymon1
-    print(generar_digipymon_aleatorio())
-
-    def menu():
-        print("Elige una opcion")
-        print("1. Buscar Digipymon") 
-        print("2. Luchar contra un entrenador")
-        print("3. Ir a la teinda")
-        print("4. Usar objeto")
-        print("5. Consultar inventario")
-        print("6. Consultar Digipymons")
-        print("7. Salir")
-        respuesta = 0
-        respuesta = input("")
-        return respuesta
+    tipos = ["fuego", "agua", "planta"]
+    nombre = lista_nombres1.obtener_nombre_digipymon()
+    vida = random.randint(10, 20)
+    ataque = random.randint(1, 10)
+    tipo = random.choice(tipos)
+    nivel = random.randint(1, 3)
+    digipymon1 = Digipymon(nombre, vida, ataque, tipo, nivel)
+    return digipymon1
     
-    def buscar_digipymon():
-        digipymon_encontrado = generar_digipymon_aleatorio()
-        probabilidad_captura = 100 - (digipymon_encontrado.nivel *10)
-        print("Has encontrado un...")
-        print(digipymon_encontrado)
-        print("La probabilidad de captura al " + digipymon_encontrado.nombre + " en de un " + str(probabilidad_captura) + "%")
+
+def menu():
+    print("Elige una opcion")
+    print("1. Buscar Digipymon") 
+    print("2. Luchar contra un entrenador")
+    print("3. Ir a la teinda")
+    print("4. Usar objeto")
+    print("5. Consultar inventario")
+    print("6. Consultar Digipymons")
+    print("7. Salir")
+    respuesta = 0
+    respuesta = input("")
+    return respuesta
     
+def buscar_digipymon(jugador, inventario):
+    digipymon_encontrado = generar_digipymon_aleatorio()
+    probabilidad_captura = 100 - (digipymon_encontrado.nivel *10)
+    print("Has encontrado un...")
+    print(digipymon_encontrado)
+    print("La probabilidad de captura al " + digipymon_encontrado.nombre + " es de un " + str(probabilidad_captura) + "%")
+    print("¿Deseas capturar al digipymon?")
+    print("1. Sí")
+    print("2. No")
+    opcion = input()
+    if opcion == "1":
+        if "digipyballs" in inventario.objetos and jugador.cantidad_dig:
+            jugador.digipyballs = jugador.digipyballs - 1
+
+    elif opcion == "2":
+        print("Has huido")
+    else:
+        print("Introudce una opción correcta")            
+
+
+    
+def main(): 
     bucle = True
     while bucle:
-        
-        respuesta = menu()
-        if respuesta == "1":
-            
-        elif respuesta == "7":
-            bucle = False
+     jugador1 = Jugador("Pepe")
+
+    
+    respuesta = menu()
+    if respuesta == "1":
+
+    elif respuesta == "7":
+        bucle = False
 
     
 main()
