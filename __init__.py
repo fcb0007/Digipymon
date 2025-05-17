70dc174bdf353b36d9a8541d34953e634ac0ed0f
Francisco Cerezo <fcb0007@alu.medac.es>
2025-05-17 21:45:55 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,6 @@
+"""
+Paquete del proyecto Digipymon, contiene todas las clases y funciones del programa
+"""
+
+__version__ = "1.0"
+__author__ = "David Jiménez Castro, Francisco Manuel Cerezo Bolívar"
