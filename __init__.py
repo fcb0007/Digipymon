73d0c641dbbd9e0bdf6aa335499db4b1f1baa91b
Francisco Cerezo <fcb0007@alu.medac.es>
2025-05-18 16:30:22 +0200

    Agrego la documentación generada

--- __init__.py
+++ __init__.py
@@ -6,9 +6,3 @@ todas las clases y funciones necesarias para la ejecución del programa
 __version__ = "1.0"
 __author__ = "David Jiménez Castro, Francisco Manuel Cerezo Bolívar"
 
-from .digipymon import Digipymon
-from .enemigo import Enemigo
-from .inventario import Inventario
-from .lista_nombres import ListaNombres
-from .jugador import Jugador
-from .main import main
